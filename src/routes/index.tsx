bda9341265b34821d1b21b2911d68dd962937891
James <jamesandrewpike@gmail.com>
2025-01-06 13:50:14 -0500

    commit

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -21,6 +21,9 @@ import { SITE } from "~/config.mjs";
 export default component$(() => {
   return (
     <>
+    <video autoplay loop id="video-background" muted>
+  <source src="https://media-us-westslateappcom.s3.us-west-1.amazonaws.com/madcowfilms/production/clips/514ebae1-ebbb-4477-addd-d52a30cd28c1-1280x720.2500.webm" type="video/webm"/>
+</video>
       <Hero />
       <Features
         highlight="Features"
